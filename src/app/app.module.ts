388b89c24ead4984a459e5a9a550d15f09f1525c
jiho <ghwlchlaks@naver.com>
2019-01-08 15:37:19 +0900

    fontawesome, icon module 추가 및 ng-bootstrap 안되는 문제 해결
    @fortawesome/ 모듈 3개 설치
    ng-bootstrap => npm install bootstrap 부트스트랩 css가 필요
    style.css에 import '~bootstrap/dist/css/bootstrap.css' 추가로 해결

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
