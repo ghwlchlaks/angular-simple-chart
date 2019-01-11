c180fabd9917d91717d22312b8255570b99659c4
ghwlchlaks <ghwlchlaks@naver.com>
2019-01-11 16:42:50 +0900

    placetop pie chart 구성
    top1,3,5,6,10,12,25 advanced pie chart 구성

--- src/app/home/home-detail/home-detail.component.ts
+++ src/app/home/home-detail/home-detail.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { d_userStats } from '../datas/d_userStats';
 import { IReturnStats, IUserStats } from '../types/t_userStats';
-import { INumberCardData } from '../types/t_ngxChart';
+import { INumberCardData, IPieData } from '../types/t_ngxChart';
 
 @Component({
   selector: 'app-home-detail',
@@ -20,6 +20,15 @@ export class HomeDetailComponent implements OnInit {
   numberCardData_solo: Array<INumberCardData> = [];
   numberCardData_duo: Array<INumberCardData> = [];
   numberCardData_squad: Array<INumberCardData> = [];
+  pieData_placeTop: Array<IPieData> = [
+    {name : 'top1', value: 0},
+    {name : 'top3', value: 0},
+    {name : 'top5', value: 0},
+    {name : 'top6', value: 0},
+    {name : 'top10', value: 0},
+    {name : 'top12', value: 0},
+    {name : 'top25', value: 0},
+  ];
 
   // total number card setup
   numberCardSingle_total: any[];
@@ -69,11 +78,22 @@ export class HomeDetailComponent implements OnInit {
   numberCardEmptyColor_squad;
   numberCardInnerPadding_squad = 0;
 
+  // advanced pie place top setup
+  pieResult_placeTop: any[];
+  pieView_placeTop: any[] = [1200, 200];
+  pieColorScheme_placeTop = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C']
+  };
+  pieGradient_placeTop = false;
+
   constructor(private route: ActivatedRoute) {
+    // number card assign
     Object.assign(this, { numberCardSingle_total: this.numberCardData_total });
     Object.assign(this, { numberCardSingle_solo: this.numberCardData_solo });
     Object.assign(this, { numberCardSingle_duo: this.numberCardData_duo });
     Object.assign(this, { numberCardSingle_squad: this.numberCardData_squad });
+    // pie assign
+    Object.assign(this, { pieResult_placeTop: this.pieData_placeTop });
   }
 
   ngOnInit() {
@@ -115,6 +135,27 @@ export class HomeDetailComponent implements OnInit {
               'name': value,
               'value': this.ApiValue.stats[value]
             });
+          } else if (index === 1 || index === 11 || index === 21) {
+            // top1_solo top1_duo top1_squad
+            this.pieData_placeTop[0].value += this.ApiValue.stats[value];
+          } else if (index === 22) {
+            // top3_squad
+            this.pieData_placeTop[1].value += this.ApiValue.stats[value];
+          } else if (index === 12) {
+            // top5_duo
+            this.pieData_placeTop[2].value += this.ApiValue.stats[value];
+          } else if (index === 23) {
+            // top6_squad
+            this.pieData_placeTop[3].value += this.ApiValue.stats[value];
+          } else if (index === 2) {
+            // top10_solo
+            this.pieData_placeTop[4].value += this.ApiValue.stats[value];
+          } else if (index === 13) {
+            // top12_duo
+            this.pieData_placeTop[5].value += this.ApiValue.stats[value];
+          } else if (index === 3) {
+            // top25_solo
+            this.pieData_placeTop[6].value += this.ApiValue.stats[value];
           }
         });
       } else {
@@ -133,6 +174,9 @@ export class HomeDetailComponent implements OnInit {
     console.log(event);
   }
   NumberCardOnSelect_squad(event) {
+    console.log(event);
+  }
+  pieOnSelect_placeTop(event) {
     console.log(event);
   }
 }
--- src/app/home/types/t_ngxChart.ts
+++ src/app/home/types/t_ngxChart.ts
@@ -2,3 +2,7 @@ export interface INumberCardData {
     name: String;
     value: Number;
 }
+export interface IPieData {
+    name: String;
+    value: Number;
+}
