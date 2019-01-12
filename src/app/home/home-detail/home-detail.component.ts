a14a7458273bee53a08b588b59a11a220bce2fbe
ghwlchlaks <ghwlchlaks@naver.com>
2019-01-12 20:11:40 +0900

    chart 데이터 초기화, 할당 , 생성 분리
    initialChartsData 차트 데이터 초기화
    initialApiData apiData초기화
    createChars chart데이터 할당
    assignChart chart생성

--- src/app/home/home-detail/home-detail.component.ts
+++ src/app/home/home-detail/home-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import { d_userStats } from '../datas/d_userStats';
 import { IReturnStats, IUserStats } from '../types/t_userStats';
@@ -112,8 +112,7 @@ export class HomeDetailComponent implements OnInit {
   // line, area
   autoScale = true;
 
-  constructor(private route: ActivatedRoute,
-    private router: Router
+  constructor(private route: ActivatedRoute
     ) {
   }
 
@@ -124,138 +123,140 @@ export class HomeDetailComponent implements OnInit {
       /*
         api call
       */
-      const numberCardData_total: Array<INumberCardData> = [];
-      const numberCardData_solo: Array<INumberCardData> = [];
-      const numberCardData_duo: Array<INumberCardData> = [];
-      const numberCardData_squad: Array<INumberCardData> = [];
-      const pieData_placeTop: Array<IPieData> = [
-        {name : 'top1', value: 0},
-        {name : 'top3', value: 0},
-        {name : 'top5', value: 0},
-        {name : 'top6', value: 0},
-        {name : 'top10', value: 0},
-        {name : 'top12', value: 0},
-        {name : 'top25', value: 0},
-      ];
-      const pieData_matchedPlayed: Array<IPieData> = [];
-      const pieData_score: Array<IPieData> = [];
+      this.initialApiData();
+      this.initialChartsData();
 
-     this.ApiData = null;
       setTimeout(() => {
-        this.ApiData = d_userStats;
+      this.ApiData = d_userStats;
       this.ApiStatus = this.ApiData.status;
       this.ApiValue = this.ApiData.value;
+
       if (this.ApiStatus) {
-        // platform
-        this.apiPlatform = this.ApiValue.platform;
-        this.apiUsername = this.ApiValue.username;
-        // lastupdate time
-        this.apiLastUpdate = this.getDate(this.ApiValue.lastupdate);
-        // totals data filter
-        Object.keys(this.ApiValue.totals).forEach((value: string, index: number) => {
-          if (Object.keys(this.ApiValue.totals).length - 1 !== index) {
-            numberCardData_total.push({
-              'name' : value,
-              'value' : this.ApiValue.totals[value]
-            });
-          }
-        });
-        // stats data filter
-        Object.keys(this.ApiValue.stats).forEach((value: string, index: number) => {
-          if (index === 0 || index === 5 || index === 6 || index === 7 || index === 8) {
-            // kill_solo kd_solo score_solo, score_solo minutedsplayed_solo
-            numberCardData_solo.push({
-              'name': value,
-              'value': this.ApiValue.stats[value]
-            });
-            if (index === 7) {
-              // grid pie score_solo
-              pieData_score.push({
-                'name': value,
-                'value': this.ApiValue.stats[value]
-              });
-            }
-          } else if (index === 10 || index === 15 || index === 16 || index === 17 || index === 18) {
-            // kill_duo kd_duo score_duo scroe_duo minutedsplayed_duo
-            numberCardData_duo.push({
-              'name': value,
-              'value': this.ApiValue.stats[value]
-            });
-            if (index === 17) {
-              // grid pie score_duo
-              pieData_score.push({
-                'name': value,
-                'value': this.ApiValue.stats[value]
-              });
-            }
-          } else if (index === 20 || index === 25 || index === 26 || index === 27 || index === 28) {
-            // kill_squad kd_squad score_squad score_squad minutedsplayed_squad
-            numberCardData_squad.push({
-              'name': value,
-              'value': this.ApiValue.stats[value]
-            });
-            if (index === 27) {
-              // grid pie score_squad
-              pieData_score.push({
-                'name': value,
-                'value': this.ApiValue.stats[value]
-              });
-            }
-          } else if (index === 1 || index === 11 || index === 21) {
-            // top1_solo top1_duo top1_squad
-            pieData_placeTop[0].value += this.ApiValue.stats[value];
-          } else if (index === 22) {
-            // top3_squad
-            pieData_placeTop[1].value += this.ApiValue.stats[value];
-          } else if (index === 12) {
-            // top5_duo
-            pieData_placeTop[2].value += this.ApiValue.stats[value];
-          } else if (index === 23) {
-            // top6_squad
-            pieData_placeTop[3].value += this.ApiValue.stats[value];
-          } else if (index === 2) {
-            // top10_solo
-            pieData_placeTop[4].value += this.ApiValue.stats[value];
-          } else if (index === 13) {
-            // top12_duo
-            pieData_placeTop[5].value += this.ApiValue.stats[value];
-          } else if (index === 3) {
-            // top25_solo
-            pieData_placeTop[6].value += this.ApiValue.stats[value];
-          } else if (index === 4 || index === 14 || index === 24) {
-            // matchesplayed_solo, matchesplayed_duo, matchesplayed_squad
-            pieData_matchedPlayed.push({
-              'name': value,
-              'value': this.ApiValue.stats[value]
-            });
-          }
-        });
-
-        this.numberCardData_total = numberCardData_total;
-        this.numberCardData_solo = numberCardData_solo;
-        this.numberCardData_duo = numberCardData_duo;
-        this.numberCardData_squad = numberCardData_squad;
-        this.pieData_placeTop = pieData_placeTop;
-        this.pieData_matchedPlayed = pieData_matchedPlayed;
-        this.pieData_score = pieData_score;
-
-        // number card assign
-        Object.assign(this, { numberCardSingle_total: this.numberCardData_total });
-        Object.assign(this, { numberCardSingle_solo: this.numberCardData_solo });
-        Object.assign(this, { numberCardSingle_duo: this.numberCardData_duo });
-        Object.assign(this, { numberCardSingle_squad: this.numberCardData_squad });
-        // pie assign
-        Object.assign(this, { pieResult_placeTop: this.pieData_placeTop });
-        Object.assign(this, { pieResult_matchedPlayed: this.pieData_matchedPlayed });
-        Object.assign(this, { pieResults_score: this.pieData_score });
+        this.createCharts(this.ApiValue);
       } else {
         console.log('error no user');
       }
       }, 3000);
     });
   }
-  private initialChartData() {
-    
+
+  private initialApiData() {
+    this.ApiData = null;
+  }
+  private initialChartsData() {
+    this.numberCardData_total = [];
+    this.numberCardData_solo = [];
+    this.numberCardData_duo = [];
+    this.numberCardData_squad = [];
+    this.pieData_placeTop = [
+      {name : 'top1', value: 0},
+      {name : 'top3', value: 0},
+      {name : 'top5', value: 0},
+      {name : 'top6', value: 0},
+      {name : 'top10', value: 0},
+      {name : 'top12', value: 0},
+      {name : 'top25', value: 0},
+    ];
+    this.pieData_matchedPlayed = [];
+    this.pieData_score = [];
+  }
+  private createCharts(ApiValue: IUserStats) {
+    // platform
+    this.apiPlatform = ApiValue.platform;
+    this.apiUsername = ApiValue.username;
+    // lastupdate time
+    this.apiLastUpdate = this.getDate(ApiValue.lastupdate);
+    // totals data filter
+    Object.keys(ApiValue.totals).forEach((value: string, index: number) => {
+      if (Object.keys(ApiValue.totals).length - 1 !== index) {
+        this.numberCardData_total.push({
+          'name' : value,
+          'value' : ApiValue.totals[value]
+        });
+      }
+    });
+    // stats data filter
+    Object.keys(this.ApiValue.stats).forEach((value: string, index: number) => {
+      if (index === 0 || index === 5 || index === 6 || index === 7 || index === 8) {
+        // kill_solo kd_solo score_solo, score_solo minutedsplayed_solo
+        this.numberCardData_solo.push({
+          'name': value,
+          'value': ApiValue.stats[value]
+        });
+        if (index === 7) {
+          // grid pie score_solo
+          this.pieData_score.push({
+            'name': value,
+            'value': ApiValue.stats[value]
+          });
+        }
+      } else if (index === 10 || index === 15 || index === 16 || index === 17 || index === 18) {
+        // kill_duo kd_duo score_duo scroe_duo minutedsplayed_duo
+        this.numberCardData_duo.push({
+          'name': value,
+          'value': ApiValue.stats[value]
+        });
+        if (index === 17) {
+          // grid pie score_duo
+          this.pieData_score.push({
+            'name': value,
+            'value': ApiValue.stats[value]
+          });
+        }
+      } else if (index === 20 || index === 25 || index === 26 || index === 27 || index === 28) {
+        // kill_squad kd_squad score_squad score_squad minutedsplayed_squad
+        this.numberCardData_squad.push({
+          'name': value,
+          'value': ApiValue.stats[value]
+        });
+        if (index === 27) {
+          // grid pie score_squad
+          this.pieData_score.push({
+            'name': value,
+            'value': ApiValue.stats[value]
+          });
+        }
+      } else if (index === 1 || index === 11 || index === 21) {
+        // top1_solo top1_duo top1_squad
+        this.pieData_placeTop[0].value += ApiValue.stats[value];
+      } else if (index === 22) {
+        // top3_squad
+        this.pieData_placeTop[1].value += ApiValue.stats[value];
+      } else if (index === 12) {
+        // top5_duo
+        this.pieData_placeTop[2].value += ApiValue.stats[value];
+      } else if (index === 23) {
+        // top6_squad
+        this.pieData_placeTop[3].value += ApiValue.stats[value];
+      } else if (index === 2) {
+        // top10_solo
+        this.pieData_placeTop[4].value += ApiValue.stats[value];
+      } else if (index === 13) {
+        // top12_duo
+        this.pieData_placeTop[5].value += ApiValue.stats[value];
+      } else if (index === 3) {
+        // top25_solo
+        this.pieData_placeTop[6].value += ApiValue.stats[value];
+      } else if (index === 4 || index === 14 || index === 24) {
+        // matchesplayed_solo, matchesplayed_duo, matchesplayed_squad
+        this.pieData_matchedPlayed.push({
+          'name': value,
+          'value': ApiValue.stats[value]
+        });
+      }
+    });
+    this.assignCharts();
+  }
+  private assignCharts() {
+    // number card assign
+    Object.assign(this, { numberCardSingle_total: this.numberCardData_total });
+    Object.assign(this, { numberCardSingle_solo: this.numberCardData_solo });
+    Object.assign(this, { numberCardSingle_duo: this.numberCardData_duo });
+    Object.assign(this, { numberCardSingle_squad: this.numberCardData_squad });
+    // pie assign
+    Object.assign(this, { pieResult_placeTop: this.pieData_placeTop });
+    Object.assign(this, { pieResult_matchedPlayed: this.pieData_matchedPlayed });
+    Object.assign(this, { pieResults_score: this.pieData_score });
   }
   NumberCardOnSelect_total(event) {
     console.log(event);
