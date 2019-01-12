61a6c13183d1707e6f1d45bb533944cce827b64b
ghwlchlaks <ghwlchlaks@naver.com>
2019-01-12 18:02:26 +0900

    open <=> closed animation 기본 구성 완료
    group query를 통해서 animation 기본 구성,
    (수정필요) home-main.component.html에 [ngStyle]인자들 component.ts에 캡슐화 필요
    (수정필요) home-main.component.html에 class 명으로 animation에서 접근하는 것을 id값으로 변경 필요

--- src/app/home/animation.ts
+++ src/app/home/animation.ts
@@ -13,12 +13,120 @@ trigger('openClose', [
     transition('open => closed', group([
         query('.jumbotron', [
             style({
-                height: '100vh',
-                backgroundColor: 'yellow',
+                height: '100vh'
             }),
-            animate(1000, style({
-                height: '30vh',
-                backgroundColor: 'green'
+            animate('1s', style({
+                height: '10vh'
+            }))
+        ]),
+        query('.container', [
+            style({
+                'padding-top': '25%',
+                'padding-left': '15%',
+                'padding-right': '15%'
+            }),
+            animate('1s', style({
+                'padding-top': '1%',
+                'padding-left': '1%',
+                'padding-right': '1%'
+            }))
+        ]),
+        query('.form-control', [
+            style({
+                'float': 'left',
+                'width': '70%',
+                'margin-left': '10px'
+            }),
+            animate('1s', style({
+                'float': 'left',
+                'width': '60%',
+                'margin-left': '10px'
+            }))
+        ]),
+        query('#platformSelect', [
+            style({
+                'float': 'left',
+                'margin-left': '10px',
+                'width': '26%'
+            }),
+            animate('1s', style({
+                'float': 'left',
+                'margin-left': '10px',
+                'width': '20%'
+            }))
+        ]),
+        query('#searchBtn', [
+            style({
+                'float': 'right',
+                'margin-left': '10px',
+                'margin-top': '1%',
+                'width': '100%'
+            }),
+            animate('1s', style({
+                'margin-left': '10px',
+                'margin-top': '0%',
+                'width': '10%'
+            }))
+        ])
+    ])),
+
+    transition('closed => open', group([
+        query('.jumbotron', [
+            style({
+                height: '100vh'
+            }),
+            animate('1s', style({
+                height: '100vh'
+            }))
+        ]),
+        query('.container', [
+            style({
+                'padding-top': '1%',
+                'padding-left': '1%',
+                'padding-right': '1%'
+            }),
+            animate('1s', style({
+                'padding-top': '25%',
+                'padding-left': '15%',
+                'padding-right': '15%'
+            }))
+        ]),
+        query('.form-control', [
+            style({
+                'float': 'left',
+                'width': '60%',
+                'margin-left': '10px'
+            }),
+            animate('1s', style({
+                'float': 'left',
+                'width': '70%',
+                'margin-left': '10px'
+            }))
+        ]),
+        query('#platformSelect', [
+            style({
+                'float': 'left',
+                'margin-left': '10px',
+                'width': '20%'
+            }),
+            animate('1s', style({
+                'float': 'left',
+                'margin-left': '10px',
+                'width': '26%'
+            }))
+        ]),
+        query('#searchBtn', [
+            style({
+                'float': 'right',
+                'margin-left': '10px',
+                'margin-top': '0%',
+                'width': '10%'
+            }),
+            animate('1s', style({
+                'float': 'right',
+                'margin-left': '10px',
+                'margin-top': '1%',
+                'width': '100%'
             }))
         ])
     ]))
@@ -57,6 +165,6 @@ trigger('openClose', [
 //       animate('1s')
 //     ]),
 //     transition('closed => open', [
-//       animate('0.5s')
+//       animate('1s')
 //     ]),
 // ]);
